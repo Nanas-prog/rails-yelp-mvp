9c3e1a02341c873e3757aa459a4eb7fb0e9d6359
anais roth <rothanais@yahoo.fr>
2019-11-13 12:48:07 +0100

    first commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+# private
+
+#   def set_restaurant
+#     @restaurant = Restaurant.find(params[:id])
+#   end
+
+#   def set_params
+#     params.require(:restaurant).permit(:name, :address, :phone, :category)
+#   end
+# end
+
+
